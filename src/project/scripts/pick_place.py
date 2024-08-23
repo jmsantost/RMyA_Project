e7cc5ee6c82669d22c45d982d6cc9e388f0117cf
José Miguel Santos <107709244+jmsantost@users.noreply.github.com>
2024-08-23 14:54:54 -0500

    Update pick_place.py

--- src/project/scripts/pick_place.py
+++ src/project/scripts/pick_place.py
@@ -4,32 +4,47 @@ import rospy
 import moveit_commander
 
 def move_to_joint_positions(group, joint_positions):
-    group.set_start_state_to_current_state()  # Actualiza el estado inicial al estado actual
+    # Actualiza el estado inicial al estado actual del grupo de articulaciones
+    group.set_start_state_to_current_state()
+    # Mueve el grupo de articulaciones a las posiciones dadas
     group.go(joint_positions, wait=True)
+    # Detiene cualquier movimiento residual
     group.stop()
 
 def close_gripper():
+    # Establece el objetivo de las articulaciones de la pinza para cerrarla
     gripper_group.set_joint_value_target([0.158, -0.158, 0.158, 0.0, 0.158, -0.158, 0.158, 0.0])
+    # Ejecuta el movimiento para cerrar la pinza
     gripper_group.go(wait=True)
+    # Detiene cualquier movimiento residual
     gripper_group.stop()
 
 def open_gripper():
+    # Establece el objetivo de la pinza en la posición "abierta"
     gripper_group.set_named_target("open")
+    # Ejecuta el movimiento para abrir la pinza
     gripper_group.go(wait=True)
+    # Detiene cualquier movimiento residual
     gripper_group.stop()
 
 def main():
+    # Inicializa la interfaz de MoveIt! para ROS
     moveit_commander.roscpp_initialize(sys.argv)
+    # Inicializa el nodo de ROS con el nombre 'move_robot_waypoints'
     rospy.init_node('move_robot_waypoints', anonymous=True)
     
     global move_group, gripper_group
+    # Define el grupo de articulaciones del brazo manipulador
     move_group = moveit_commander.MoveGroupCommander("manipulator")
+    # Define el grupo de articulaciones de la pinza
     gripper_group = moveit_commander.MoveGroupCommander("gripper")
     
+    # Establece los factores de escala de velocidad y aceleración máxima
     move_group.set_max_velocity_scaling_factor(0.1)
     move_group.set_max_acceleration_scaling_factor(0.1)
     
-    waypoints = [
+    # Puntos desde HOME (dónde todos los joints son 0) hasta la botella.
+    hometobottle = [
         [-0.722, -0.616, 0.0, 0.0, 0.0, 0.0],
         [-1.320, -0.616, 0.0, 0.0, 0.0, 0.0],
         [-1.320, -0.616, -0.757, -1, -1.5, 0.0],
@@ -38,40 +53,44 @@ def main():
         [-1.317, -0.654, 0.413, -1.323, -1.5, 0.229], 
     ]
     
-    for position in waypoints:
+    # Mueve el brazo siguiedno la trayectoria definida de hometobottle
+    for position in hometobottle:
         move_to_joint_positions(move_group, position)
         rospy.sleep(1)
 
+    # Cierra la pinza
     close_gripper()
     rospy.sleep(2)
 
-    new_waypoints = [
+    # Puntos desde la botetlla hasta la caja.
+    bottletobox = [
        [-1.317, -0.8, 0.413, -1.323, -1.5, 0.229],  
        [0.757, -0.8, 0.413, -1.323, -1.5, 0.229],
        [0.793, -0.531, 1, -2.162, -1.504, 0.267],
-      
     ]
     
-    for position in new_waypoints:
+    # Mueve el brazo siguiedno la trayectoria definida de bottletobox
+    for position in bottletobox:
         move_to_joint_positions(move_group, position)
         rospy.sleep(1)
 
-    # Abrir la pinza
+    # Abre la pinza
     open_gripper()
     rospy.sleep(2)
 
-    nuevo_waypoint_1 = [
-                        [0.793, -0.898, 1, -2.162, -1.504, 0.267],
-                        [0, -0.898, 1, -2.162, -1.504, 0.267],
-                        [0, 0,0,0,0,0] ,
-                        
+    # Puntos desde Box hacia Home.
+    boxtohome= [
+        [0.793, -0.898, 1, -2.162, -1.504, 0.267],
+        [0, -0.898, 1, -2.162, -1.504, 0.267],
+        [0, 0, 0, 0, 0, 0],
     ]
-    for position in nuevo_waypoint_1:
+    
+    #Mueve el brazo siguiedno la trayectoria definida de boxtohome
+    for position in boxtohome:
         move_to_joint_positions(move_group, position)
         rospy.sleep(1)
 
-
-
+    # Cierra la interfaz de MoveIt! para ROS
     moveit_commander.roscpp_shutdown()
 
 if __name__ == "__main__":

0aa820ea26a9f64b0606cf04af87680df730e614
José Miguel Santos <107709244+jmsantost@users.noreply.github.com>
2024-08-23 14:56:33 -0500

    Update pick_place.py

--- src/project/scripts/pick_place.py
+++ src/project/scripts/pick_place.py
@@ -4,46 +4,46 @@ import rospy
 import moveit_commander
 
 def move_to_joint_positions(group, joint_positions):
-    # Actualiza el estado inicial al estado actual del grupo de articulaciones
+    # Update the initial state to the current state of the joint group
     group.set_start_state_to_current_state()
-    # Mueve el grupo de articulaciones a las posiciones dadas
+    # Move the joint group to the specified positions
     group.go(joint_positions, wait=True)
-    # Detiene cualquier movimiento residual
+    # Stop any residual motion
     group.stop()
 
 def close_gripper():
-    # Establece el objetivo de las articulaciones de la pinza para cerrarla
+    # Set the target joint positions for the gripper to close it
     gripper_group.set_joint_value_target([0.158, -0.158, 0.158, 0.0, 0.158, -0.158, 0.158, 0.0])
-    # Ejecuta el movimiento para cerrar la pinza
+    # Execute the motion to close the gripper
     gripper_group.go(wait=True)
-    # Detiene cualquier movimiento residual
+    # Stop any residual motion
     gripper_group.stop()
 
 def open_gripper():
-    # Establece el objetivo de la pinza en la posición "abierta"
+    # Set the target of the gripper to the "open" position
     gripper_group.set_named_target("open")
-    # Ejecuta el movimiento para abrir la pinza
+    # Execute the motion to open the gripper
     gripper_group.go(wait=True)
-    # Detiene cualquier movimiento residual
+    # Stop any residual motion
     gripper_group.stop()
 
 def main():
-    # Inicializa la interfaz de MoveIt! para ROS
+    # Initialize the MoveIt! interface for ROS
     moveit_commander.roscpp_initialize(sys.argv)
-    # Inicializa el nodo de ROS con el nombre 'move_robot_waypoints'
+    # Initialize the ROS node with the name 'move_robot_waypoints'
     rospy.init_node('move_robot_waypoints', anonymous=True)
     
     global move_group, gripper_group
-    # Define el grupo de articulaciones del brazo manipulador
+    # Define the joint group for the manipulator arm
     move_group = moveit_commander.MoveGroupCommander("manipulator")
-    # Define el grupo de articulaciones de la pinza
+    # Define the joint group for the gripper
     gripper_group = moveit_commander.MoveGroupCommander("gripper")
     
-    # Establece los factores de escala de velocidad y aceleración máxima
+    # Set the maximum velocity and acceleration scaling factors
     move_group.set_max_velocity_scaling_factor(0.1)
     move_group.set_max_acceleration_scaling_factor(0.1)
     
-    # Puntos desde HOME (dónde todos los joints son 0) hasta la botella.
+    # Waypoints from HOME (where all joints are 0) to BOTTLE.
     hometobottle = [
         [-0.722, -0.616, 0.0, 0.0, 0.0, 0.0],
         [-1.320, -0.616, 0.0, 0.0, 0.0, 0.0],
@@ -53,44 +53,44 @@ def main():
         [-1.317, -0.654, 0.413, -1.323, -1.5, 0.229], 
     ]
     
-    # Mueve el brazo siguiedno la trayectoria definida de hometobottle
+    # Move the arm following the defined trajectory from hometobottle
     for position in hometobottle:
         move_to_joint_positions(move_group, position)
         rospy.sleep(1)
 
-    # Cierra la pinza
+    # Close the gripper
     close_gripper()
     rospy.sleep(2)
 
-    # Puntos desde la botetlla hasta la caja.
+    # Waypoints from the BOTTLE to BOX.
     bottletobox = [
        [-1.317, -0.8, 0.413, -1.323, -1.5, 0.229],  
        [0.757, -0.8, 0.413, -1.323, -1.5, 0.229],
        [0.793, -0.531, 1, -2.162, -1.504, 0.267],
     ]
     
-    # Mueve el brazo siguiedno la trayectoria definida de bottletobox
+    # Move the arm following the defined trajectory from bottletobox
     for position in bottletobox:
         move_to_joint_positions(move_group, position)
         rospy.sleep(1)
 
-    # Abre la pinza
+    # Open the gripper
     open_gripper()
     rospy.sleep(2)
 
-    # Puntos desde Box hacia Home.
+    # Waypoints from the BOX to HOME.
     boxtohome= [
         [0.793, -0.898, 1, -2.162, -1.504, 0.267],
         [0, -0.898, 1, -2.162, -1.504, 0.267],
         [0, 0, 0, 0, 0, 0],
     ]
     
-    #Mueve el brazo siguiedno la trayectoria definida de boxtohome
+    # Move the arm following the defined trajectory from boxtohome
     for position in boxtohome:
         move_to_joint_positions(move_group, position)
         rospy.sleep(1)
 
-    # Cierra la interfaz de MoveIt! para ROS
+    # Shut down the MoveIt! interface for ROS
     moveit_commander.roscpp_shutdown()
 
 if __name__ == "__main__":
